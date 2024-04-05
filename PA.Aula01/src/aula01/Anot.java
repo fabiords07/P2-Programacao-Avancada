ebc3296f1095f1c3811a6b45279944ab92abf6cb
Fábio Rangel <142637818+fabiords07@users.noreply.github.com>
2024-04-04 22:09:28 -0300

    Add files via upload

---
+++ PA.Aula01/src/aula01/Anot.java
@@ -0,0 +1,56 @@
+package aula01;
+
+public class Anot {
+	
+//	String x = "10";
+//	int y= Integer.parseInt(x);
+//	
+//	String xx = "20.54";
+//	double yy = Double.parseDouble(x);
+	
+//	? valor, se true
+//	: valor, se false
+//	teste para fazer antes de do slide de matriz
+	
+//for (inicialização; condição de continuação; incremento/decremento) => vide exercicio 06
+// Corpo do loop
+// Código a ser executado repetidamente
+	
+// while (condição)
+	// { instruções; }
+
+// do
+	// {instruções }
+	//while (condição);
+	
+// switch (seletor inteiro) => não da para colocar maior que menor que
+// case valor inteiro:
+	
+// array => vide exercicio 09 aula 2 / 
+// int[] num = {10, 20, 30};    System.out.println(num.length);
+// criação sem inicialização de arrays: String [ ] nomes = new String[4];
+// matriz em matriz, primeiro linha, depois coluna
+// criação sem inicialização int[ ] [ ] matriz = new int[3] [2];
+	
+// camada controller recebe a requisição primeiro dentro da estrutura de MVC
+// controller = intermediar as duas camadas
+	
+//	MVC é um padrão arquitetural comumente utilizado no desenvolvimento de aplicativos 
+//	de software, especialmente em aplicativos da web, que divide uma aplicação em três 
+//	componentes principais (Model, View e Controller), cada um com responsabilidades 
+//	específicas, promovendo assim a modularidade, a organização e a flexibilidade do código.
+	
+//	Model (coração da aplicação) 
+//	▪ Regras de negocio 
+//	▪ Entidades 
+//	▪ Camada de acesso a dados 
+//	▪ Tudo o que pertence a sua aplicação em termo de regras de negócio e validação
+	
+//	View (Tudo o que diz respeito a visualizar a sua aplicação. As telas estão relacionadas com esta camada ) 
+//	▪ Seja desktop/web ▪ É responsável por renderizar a página com resposta a requisição
+	
+//	Controller (coordena o fluxo de dados do model para o view. 
+//	Ele garante que a lógica de negócios seja separada da lógica de apresentação.
+	
+//	handler mapping = Mapeador de requisições: determina qual controller vai responder determinada definição 
+}
